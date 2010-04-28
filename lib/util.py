34aa65dd5d9eaeeb58ad1e2d38b592e2680d5dc1
Thomas A Caswell <tcaswell@uchicago.edu>
2010-04-28 17:26:31 -0500

    Added query function to util
    finished add_data_sets functions

--- lib/util.py
+++ lib/util.py
@@ -15,6 +15,7 @@
 #You should have received a copy of the GNU General Public License
 #along with this program; if not, see <http://www.gnu.org/licenses>.
 
+import sys
 
 class Cords3D:
     """A class for carrying around sets of 3D data"""
@@ -59,3 +60,37 @@ class cord_pairs:
         self.y = y
     def __iter__(self):
         return itertool.izip(x,y)
+
+## {{{ http://code.activestate.com/recipes/577058/ (r2)
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+    
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is one of "yes" or "no".
+    """
+    valid = {"yes":"yes",   "y":"yes",  "ye":"yes",
+             "no":"no",     "n":"no"}
+    if default == None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while 1:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return default
+        elif choice in valid.keys():
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "\
+                             "(or 'y' or 'n').\n")
+## end of http://code.activestate.com/recipes/577058/ }}}
--- scripts/add_data_sets.py
+++ scripts/add_data_sets.py
@@ -21,6 +21,7 @@ import lib.general as gen
 import trackpy.cpp_wrapper as cw
 from datetime import datetime
 from datetime import date
+import os.path
 import re
 
 _base_path = '/home/tcaswell/colloids/data/polyNIPAM_batch_12/'
@@ -28,32 +29,92 @@ _base_path = '/home/tcaswell/colloids/data/polyNIPAM_batch_12/'
 def check_existance(conn,fname):
     '''Checks the connection to see if there is already an entry for
     this filename.  Returns bool'''
-    res = sq.execute("select key from dsets where fname")
-
-    return True
+    res = conn.execute("select key from dsets where fname")
+    if res.rowcount >0:
+        return True
+    
+    return False
 
 def guess_temp(fname):
     """Parses the file name and attempts to guess the temperature"""
-    pass
+    fname = fname.split('/')[-1]
+    match = re.findall('\d\d-\d',fname)
+    if len(match) >0:
+        return min(map(lambda x: float(x.replace('-','.')),match))
+
+    
+    match = re.findall('\d\d_\d',fname)
+    if len(match) >0:    
+        return min(map(lambda x: float(x.replace('_','.')),match))
+
+
+    match = re.findall('\d\d-',fname)
+    if len(match) >0:
+        return min(map(lambda x: float(x.replace('-','')),match))
+
+    match = re.findall('\d\d_',fname)
+    if len(match) >0:
+        return min(map(lambda x: float(x.replace('_','')),match))
+
+    return None
+
 
 def ask_temp(fname):
     """asks the user for the value of the temperature"""
-    pass
+    print "fname: " + fname
+    temp = raw_input("enter temperature or None: ")
+    try:
+        return float(temp)
+    except:
+        return None
+
 def guess_dtype(fname):
     """Parse the file name and attempt to guess the type"""
+    if fname.find('ztl') != -1:
+        return 'ztl'
+    elif fname.find('z')!=-1:
+        return 'z'
+    elif fname.find('warming')!=-1:
+        return 'ramp'
+    else:
+        return 't'
+    
     pass
 
 def ask_dtype(fname):
     """Asks the user for the dtype"""
+    print "fname: " +  fname
+    menu = { 1: 't',
+             2: 'z',
+             3: 'ramp',
+             4: 'ztl',
+             5: 'None'}
+    
+
+    for m in menu:
+        print m,menu[m]
+    while True:
+        dt = raw_input("enter type: ")
+        try:
+            dt = int(dt)
+            if menu.has_key(dt):
+                return menu[dt]
+        except:
+            pass
     pass
 
 def guess_sname(fname):
     """Parse the file name and attempt to guess the sample name"""
     fsplit = fname.split('/')
     if len(fsplit) > 2:
-        return fsplit[1]
+        sname = fsplit[1]
+        if fsplit[-1].find('thin') != -1:
+            sname = sname + '-thin'
+
     else:
-        ask_sname(fname)
+        sname = ask_sname(fname)
+
+    return sname
 
 def ask_sname(fname):
     """Asks the user for the dtype"""
@@ -67,7 +128,7 @@ def guess_date(fname):
         return datetime.strptime(fsplit[0],'%Y%m%d').date().isoformat()
     if re.match('\d{4}-\d{2}-\d{2}',fsplit[0]):
         return datetime.strptime(fsplit[0],'%Y-%m-%d').date().isoformat()
-        
+
     return ask_date(fname)
 
 def ask_date(fname):
@@ -86,13 +147,74 @@ def ask_date(fname):
 
 def display_meta_data(fname,dtype,sname,temp,date):
     """Displays a nicely formatted output of meta data for confirmation"""
-    pass
+    print "fname: " + fname
+    print 'dtype: '+str(dtype)
+    print 'sname: '+str(sname)
+    print 'temp: ' +str( temp)
+    print 'date: ' +str( date)
+
+    
 
 def strip_base(fname):
     """Takes off a base path the path names"""
-    return fname.replace(_base_path,'')
+    return os.path.realpath(fname).replace(_base_path,'')
     
 def guess_meta_data(fname):
     """Calls the specific functions above"""
-    return guess_dtype(fname),guess_sname(fname),guess_temp(fname),guess_date
+    return guess_dtype(fname),guess_sname(fname),guess_temp(fname),guess_date(fname)
 
+def ask_meta_data(fname,dtype,sname,temp,date):
+    """asks if the values are correct and queries for replacements"""
+    print "fname: " + fname
+    query_fun(fname,'dtype',dtype,ask_dtype)
+    query_fun(fname,'sname',sname,ask_sname)
+    query_fun(fname,'temp',temp,ask_temp)
+    query_fun(fname,'date',date,ask_date)
+
+    return dtype,sname,temp,date
+    
+def query_fun(fname,key,value,func):
+    """asks if the value is correct for the key, if not, calls func to fix it"""
+    print key +": "+ str(value)
+    resp = util.query_yes_no('is this correct')
+    if resp == 'no':
+        value = func(fname)
+
+    return value
+
+def extract_md(fname):
+    """takes a guess at parsing, asks for validation, returns tuple"""
+    fname = strip_base(fname)
+    md = guess_meta_data(fname)
+    display_meta_data(fname, *md)
+    resp = util.query_yes_no('are these values correct')
+    if resp == 'yes':
+        return md
+    elif resp == 'no':
+        return ask_meta_data(fname,*md)
+
+def process_fname(conn,fname):
+    """Take in a connection and a database, parse the fname and enter into database"""
+    fname = os.path.realpath(fname)
+    if check_existance(conn,fname):
+        raise "already exists in database"
+    md = extract_md(fname)
+    print (fname,) + md
+    conn.execute('insert into dsets (fname,dtype,sname,temp,date) values (?,?,?,?,?)',(fname,) + md)
+    conn.commit()
+
+def visit(conn,dirname,names):
+    """Function for walk """
+
+    for f in names:
+        fname = dirname + '/'+f
+        if os.path.isfile(fname):
+            (base,ext) = os.path.splitext(fname)
+            if ext == '.tif':
+                multi_f = re.findall('file\d{3}',f)
+                if len(multi_f) ==0:
+                    process_fname(conn,fname)
+            
+def dir_loop(path,conn):
+    """Path is the top level directory to look in, uses walk """
+    os.path.walk(path,visit,conn)
