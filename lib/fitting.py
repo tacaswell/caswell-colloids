f591f2f9a7e2fffde2c9606596b2be2daf3897ee
Thomas A Caswell <tcaswell@uchicago.edu>
2010-05-10 10:35:22 -0500

    Changed fitting code to use much better fitting functions in scipy
    changed some function definitions to match what it expects

--- lib/fitting.py
+++ lib/fitting.py
@@ -16,6 +16,7 @@
 #along with this program; if not, see <http://www.gnu.org/licenses>.
 
 from util import cord_pairs
+import scipy.odr as sodr
 import numpy as np
 import scipy.optimize
 import general
@@ -45,24 +46,24 @@ def _trim_gofr(gofr,r0):
 
     return gofr_trim
 
-def fun_decay_exp_inv(r,p):
+def fun_decay_exp_inv(p,r):
     """Returns C/r exp(- r/a) cos(K(r)+phi_0) + m r + b
     evaluated at r.  p = (a,K,C,phi_0,m,b)"""
     return (p[2] / r) * np.exp(-r/p[0]  ) * np.cos(p[1]*r + p[3])+ r*p[4] + p[5]
 
-def fun_decay_exp_inv_dr(r,p):
+def fun_decay_exp_inv_dr(p,r):
     """ d(C/r exp(- r/a) cos(K(r)+phi_0) + m r + b)/dr
     evaluated at r.  p = (a,K,C,phi_0,m,b)"""
     return p[4] + (np.exp(-(r/p[0]))* (-p[2]* (p[0] + r)* np.cos(p[3] + p[1]* r) - p[0] *p[2]* p[1]* r* np.sin(p[3] + p[1]* r)))/( p[0]* r**2)
 
 
-def fun_decay_exp(r,p):
+def fun_decay_exp(p,r):
     """Returns C exp(- r/a) cos(K(r)+phi_0)
     evaluated at r.  p = (a,K,C,phi_0)"""
     return (p[2] ) * np.exp(-r/p[0]  ) * np.cos(p[1]*r + p[3]) + (r)*p[4] + p[5]
 
 
-def fun_decay_inv(r,p):
+def fun_decay_inv(p,r):
     """Returns C/r cos(K(r)+phi_0)
     evaluated at r.  p = (K,C,phi_0)"""
     return (p[1] / r) * np.cos(p[0]*r + p[2])
@@ -72,7 +73,7 @@ def fit_gofr(gofr,r0,func,p0 =(1,12,.5,0,0,0)):
     """
     Fits to 
     Takes in gofr as a cord_pairs and r0 in
-    units of 1/d, d as determined by the location
+    units of d, d as determined by the location
     of the first peak.
 
     Returns the tuple that is the argument for the function
@@ -84,7 +85,7 @@ def fit_gofr(gofr,r0,func,p0 =(1,12,.5,0,0,0)):
     
 
     def local_fun(p):
-        return func(gofr.x,p) - gofr.y+1
+        return func(p,gofr.x) - gofr.y+1
 
     (p_out,p_cov,id,m,flg) = scipy.optimize.leastsq(local_fun,p0,full_output=1)
     print flg
@@ -92,18 +93,40 @@ def fit_gofr(gofr,r0,func,p0 =(1,12,.5,0,0,0)):
     # shove into the numpy fitting code
 
 
-def fit_tmp_series_gofr2D(sname,conn):
+def fit_gofr2(gofr,r0,func,p0=(1,12,.5,0,0,0)):
+    """
+    Fits to 
+    Takes in gofr as a cord_pairs and r0 in
+    units of d, d as determined by the location
+    of the first peak.
+
+    Returns the tuple that is the argument for the function
+    used for the fitting, the covariance matrix, and the
+    sum of the residual squared
+    """
+    # trim the g(r) data
+    gofr = _trim_gofr(gofr,r0)
+    
+    
+    data = sodr.Data(gofr.x,gofr.y)
+    model = sodr.Model(func)
+    worker = sodr.ODR(data,model,p0)
+    out = worker.run()
+
+    return out
+    
+
+
+def fit_tmp_series_gofr2D(res,conn):
     '''Takes in a sample name and fits all of the 2D gofrs'''
     
     
-    res = conn.execute("select comps.comp_key,comps.fout,dsets.temp from comps,dsets where comps.dset_key = dsets.key and comps.function='gofr' and dsets.sname = ? and dsets.dtype = 't'",(sname,)).fetchall()
-
 
     temps = []
     fits = []
     for r in res:
         gofr = general.get_gofr2D(r[0],conn)
-        fits.append(fit_gofr(gofr,2,fun_decay_exp_inv,(2,7.35,1.5,0,0,0)))
+        fits.append(fit_gofr2(gofr,2,fun_decay_exp,(2,7.35,1.5,0,0,1)))
         try:
             temps.append(float(r[2]))
         except (ValueError,TypeError ) :
