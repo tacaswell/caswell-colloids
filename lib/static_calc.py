43d056c76ca990e0e16da5a87e9e9a0c81bf5fa9
Thomas A Caswell <tcaswell@uchicago.edu>
2010-06-03 16:59:15 -0500

    added a python version of g(r)
    added a script to do computations based on date

---
+++ lib/static_calc.py
@@ -0,0 +1,94 @@
+#Copyright 2010 Thomas A Caswell
+#tcaswell@uchicago.edu
+#http://jfi.uchicago.edu/~tcaswell
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 3 of the License, or (at
+#your option) any later version.
+#
+#This program is distributed in the hope that it will be useful, but
+#WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, see <http://www.gnu.org/licenses>.
+from __future__ import division
+import numpy as np
+
+class gofr_comp:
+    def __init__(self,nbins,max_r):
+        self.nbins = nbins
+        self.vals = np.zeros(self.nbins)
+        self.max_r = max_r
+        self.min_r = 0
+        self.bin_edges = np.linspace(self.min_r,self.max_r,self.nbins+1)
+        self.add_count = 0
+        self.scaleing = self.nbins /(self.max_r - self.min_r)
+    def add_particle(self,cord,list):
+        """Adds a particle to the g(r) computation by taking distances
+        against list"""
+
+        cord = np.array(cord)
+        
+        for p in list:
+            p = np.array(p)
+            r = np.sqrt(np.sum((p-cord)**2))
+            
+            if r >= self.max_r or r < self.min_r or r ==0:
+                continue
+            r -= self.min_r
+            
+            self.vals[np.floor(r*self.scaleing)]+=1
+
+        self.add_count+=1
+
+
+def indx_to_cord_2D(indx,hash_size):
+
+    if indx <0 or (not indx < np.prod(hash_size) ):
+        raise "Index out of range"
+    
+    return (indx%hash_size[0],indx//hash_size[0])
+    
+def cord_to_indx_2D(cord,hash_size):
+    """Converts coordinate position to an index.  """
+    assert len(cord) == len(hash_size)
+    cord = np.array(cord)
+    if any(cord  >= hash_size) or any(cord < 0):
+        print cord
+        print hash_size
+        raise "cord out of range"
+    return int(cord[0] + cord[1] * hash_size[0])
+
+def hash_fun(cord,box_size,hash_size):
+    return cord_to_indx_2D(np.floor(np.array(cord)/box_size),hash_size)
+
+
+
+class hash_case:
+    def __init__(self,dims,box_size):
+        self.dims = dims
+        self.hash_dims = np.ceil(np.array(dims)/box_size)
+        self.box_size = box_size
+        self.hash_table = [[] for j in range(int(np.prod(self.hash_dims)))]
+    def get_region(self,center, rrange):
+        center = np.array(center)
+        region = []
+        shifts = [np.array([j,k]) for j in range(-rrange,rrange + 1) for k in range(-rrange,rrange + 1)]
+        boxes =  [region.extend(self.hash_table[cord_to_indx_2D(center + s,self.hash_dims)]) for s in shifts]
+        return region
+    def add_particle(self,cord):
+        self.hash_table[hash_fun(cord,self.box_size,self.hash_dims)].append(cord)
+        
+    def compute_corr(self,buf,fun):
+        centers = [(j,k)
+                   for j in range(buf,int(self.hash_dims[0]-buf))
+                   for k in range(buf,int(self.hash_dims[1]-buf))]
+        for c in centers:
+            print c
+            cur_box = self.hash_table[cord_to_indx_2D(c,self.hash_dims)]
+            cur_region = self.get_region(c,buf)
+            for p in cur_box:
+                fun(p,cur_region)
---
+++ scripts/do_gofr_date.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+#Copyright 2010 Thomas A Caswell
+#tcaswell@uchicago.edu
+#http://jfi.uchicago.edu/~tcaswell
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 3 of the License, or (at
+#your option) any later version.
+#
+#This program is distributed in the hope that it will be useful, but
+#WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, see <http://www.gnu.org/licenses>.
+
+
+
+import lib.general as gen
+import trackpy.cpp_wrapper as cw
+
+def gofr_date(conn,date):
+
+    res_t = conn.execute("select key from dsets where date = ? and dtype = 't'",(date,)).fetchall()
+
+
+    for r in res_t:
+        print r[0]
+        cw.do_gofr(r[0],conn)
+
+    ## res_z = conn.execute("select key from dsets where date = ? and dtype = 'z'",(date,)).fetchall()
+    ## for r in res_z:
+    ##     continue
+    ##     print r[0]
+    ##     cw.do_gofr(r[0],conn)
+    ##     cw.do_link3D(r[0],conn)
+    ##     cw.do_gofr3D(r[0],conn)
+
+def main_loop():
+    conn = gen.open_conn();
+    gofr_date(conn,'2010-05-24')
+
+    conn.close()
+
+if __name__ == "__main__":
+    main_loop()
+
