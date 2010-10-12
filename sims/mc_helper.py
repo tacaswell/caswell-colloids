262771b7337851f3201a59cd6fef1e7917065a7d
Thomas A Caswell <tcaswell@uchicago.edu>
2010-10-12 17:17:20 -0500

    First pass at basic Monte Carlo for making dense clusters to test my theories about the spacing in g(r)

---
+++ sims/mc_helper.py
@@ -0,0 +1,119 @@
+#Copyright 2010 Thomas A Caswell
+#tcaswell@uchicago.edu
+#http://jfi.uchicago.edu/~tcaswell
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 3 of the License, or (at
+#your option) any later version.
+#
+#This program is distributed in the hope that it will be useful, but
+#WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, see <http://www.gnu.org/licenses>.
+from __future__ import division
+import numpy as np
+import numpy.random as nr
+
+
+def run_steps(start,max_accepted_steps,max_steps,generate_step,log_p_fun):
+    '''Runs MC until there are max_accepted_steps accepted steps or until
+    there are max_steps total tried.
+
+    The simulation is started at the point start
+
+    the function log_p_fun returns the log of the probability density.
+
+    Proposed steps are generate using generate step'''
+
+    accpted_steps = 0
+    steps = 0
+    cur_pos = start
+    cur_log_p = log_p_fun(start)
+    step_chain = [(cur_pos,cur_log_p)]
+    
+    # main loop
+    while (max_accepted_steps > accpted_steps) and (max_steps > steps):
+        # proposed step
+        prop_pos = generate_step(cur_pos)
+
+        # get the ratio of probability for the two positions 
+        prop_log_p = log_p_fun(prop_pos)
+
+        
+        
+        if prop_log_p > cur_log_p:
+            p = 1
+        else:
+            p = np.exp(prop_log_p - cur_log_p)
+        
+        # get a uniform random number
+        u = nr.uniform(0,1)
+        
+        # if to accept or not
+        if u<p:
+            step_chain.append((prop_pos,prop_log_p))
+            cur_pos = prop_pos
+            cur_log_p = prop_log_p
+            accpted_steps +=1
+        # increment steps
+        steps += 1
+
+    print accpted_steps,steps
+    return step_chain
+
+def p_fun_generator(p_list,T,ip_fun,g_scale):
+    '''Generates probability densities for a single particle being
+    added to the cluster, operates with k = 1 and temperature T.
+
+    uses ip_fun for inter particle potential and a linear potential to
+    attract everything to the origin.
+
+    the value of r^2 is passed in to ip_fun to same some calculation time.
+
+    '''
+
+    def log_p_fun(pos):
+        r_s = [np.sum((p-pos)**2) for p in p_list]
+        return -(np.sum([ip_fun(r) for r in r_s ])) /T
+
+    return log_p_fun
+
+
+def lj_generator(ep,rmin):
+    '''
+    Generates Lenard-Jones penitential functions for use in p_fun_generator
+
+    ep is the over all energy scale, rmin is the location of the minimum
+    
+    '''
+    def lj(r):
+        r_tmp = np.power(rmin**2/r,3)
+        return ep*(np.power(r_tmp,2) - 2 * r_tmp)
+    
+
+    return lj
+
+def gen_step(pos):
+    '''Generates the next step '''
+
+    return pos + nr.normal(0,.05,np.shape(pos))
+
+
+def list_loop(p_list,n,lj):
+    for j in range(0,n):
+        lpf = p_fun_generator(p_list,.1,lj,1);
+        prop_point = nr.uniform(-1,1,3)
+        prop_point = prop_point/np.sqrt(np.sum(prop_point**2))*pow(len(p_list)/4,1.0/3)
+        chain = run_steps(prop_point,5000,500000,gen_step,lpf)
+        p_list.append(np.array([np.mean([c[0][0] for c in chain[1000:]]),
+                                np.mean([c[0][1] for c in chain[1000:]]),
+                                np.mean([c[0][2] for c in chain[1000:]])]))
+                                                                        
+    return p_list
+                                                                        
+ 
+
