4e9636df51b2572519541503f08694c5ab6bf2f2
Thomas A Caswell <tcaswell@uchicago.edu>
2010-11-18 12:55:47 -0600

    Added sql script to geneterate the new database

---
+++ db_refactor/generate_database.sql
@@ -0,0 +1,140 @@
+CREATE TABLE dsets (
+	dset_key INTEGER PRIMARY KEY ASC AUTOINCREMENT,
+        fname TEXT NOT NULL UNIQUE, 
+        dtype TEXT NOT NULL,
+        ddate DATE NOT NULL,
+        sname TEXT NOT NULL
+);
+
+CREATE TABLE comps(
+	comp_key INTEGER PRIMARY KEY ASC AUTOINCREMENT,
+	func_name TEXT NOT NULL,
+	dset_key INTEGER,
+	FOREIGN KEY(dset_key) REFERENCES dsets(dset_key)
+);
+
+CREATE TABLE iden(
+	comp_key INTEGER PRIMARY KEY,
+	dset_key INTEGER NOT NULL,
+	threshold FLOAT NOT NULL,
+	hwhm FLOAT NOT NULL,
+	p_rad INTEGER NOT NULL,
+	d_rad INTEGER NOT NULL,
+	mask_rad INTEGER NOT NULL,
+        top_cut FLOAT NOT NULL,
+	frames_avged INTEGER NOT NULL,
+	FOREIGN KEY(dset_key) REFERENCES dsets(dset_key),
+	FOREIGN KEY(comp_key) REFERENCES comps(comp_key)
+);
+
+CREATE TABLE gofr (
+       comp_key INTEGER PRIMARY KEY,
+       dset_key INTEGER NOT NULL,
+       iden_key INTEGER NOT NULL,
+       nbins INTEGER NOT NULL,
+       max_range FLOAT NOT NULL,
+       shift_cut FLOAT ,
+       rg_cut FLOAT ,
+       e_cut FLOAT ,
+       FOREIGN KEY(dset_key) REFERENCES dsets(dset_key),
+       FOREIGN KEY(comp_key) REFERENCES comps(comp_key),
+       FOREIGN KEY(iden_key) REFERENCES iden(comp_key)
+);
+
+CREATE TABLE gofr_by_plane(
+       comp_key INTEGER PRIMARY KEY,
+       dset_key INTEGER NOT NULL,
+       iden_key INTEGER NOT NULL,
+       nbins INTEGER NOT NULL,
+       max_range FLOAT NOT NULL,
+       comp_count INTEGER NOT NULL,
+       shift_cut FLOAT ,
+       rg_cut FLOAT ,
+       e_cut FLOAT ,
+       FOREIGN KEY(dset_key) REFERENCES dsets(dset_key),
+       FOREIGN KEY(comp_key) REFERENCES comps(comp_key),
+       FOREIGN KEY(iden_key) REFERENCES iden(comp_key)
+);
+
+CREATE TABLE tracking (
+       comp_key INTEGER PRIMARY KEY,
+       iden_key INTEGER NOT NULL,  
+       dset_key INTEGER NOT NULL,
+       search_range FLOAT NOT NULL,
+       shift_cut FLOAT ,
+       rg_cut FLOAT ,
+       e_cut FLOAT ,
+       FOREIGN KEY(dset_key) REFERENCES dsets(dset_key),
+       FOREIGN KEY(comp_key) REFERENCES comps(comp_key),
+       FOREIGN KEY(iden_key) REFERENCES iden(comp_key)
+);
+
+CREATE TABLE msd (
+       comp_key INTEGER PRIMARY KEY,
+       iden_key INTEGER NOT NULL,
+       dset_key INTEGER NOT NULL,
+       search_range FLOAT NOT NULL,
+       msd_steps INTEGER NOT NULL,
+       min_track_length INTEGER NOT NULL,
+       shift_cut FLOAT ,
+       rg_cut FLOAT ,
+       e_cut FLOAT ,
+       FOREIGN KEY(dset_key) REFERENCES dsets(dset_key),
+       FOREIGN KEY(comp_key) REFERENCES comps(comp_key),
+       FOREIGN KEY(iden_key) REFERENCES iden(comp_key)
+);
+
+CREATE TABLE msd_trk (
+       comp_key INTEGER PRIMARY KEY,
+       trk_key INTEGER NOT NULL,
+       dset_key INTEGER NOT NULL,
+       msd_steps INTEGER NOT NULL,
+       min_track_length INTEGER NOT NULL,
+       FOREIGN KEY(dset_key) REFERENCES dsets(dest_key),
+       FOREIGN KEY(comp_key) REFERENCES comps(comp_key),
+       FOREIGN KEY(trk_key) REFERENCES tracking(comp_key)
+);
+
+CREATE TABLE trk_stat (
+       comp_key INTEGER PRIMARY KEY,
+       iden_key INTEGER NOT NULL,
+       dset_key INTEGER NOT NULL,
+       search_range FLOAT NOT NULL,
+       steps INTEGER NOT NULL,
+       hist_bins INTEGER NOT NULL,
+       hist_range FLOAT NOT NULL,
+       shift_cut FLOAT ,
+       rg_cut FLOAT ,
+       e_cut FLOAT ,
+       FOREIGN KEY(dset_key) REFERENCES dsets(dset_key),
+       FOREIGN KEY(comp_key) REFERENCES comps(comp_key),
+       FOREIGN KEY(iden_key) REFERENCES iden(comp_key)
+);
+
+
+CREATE TABLE trk_stat_trk (
+       comp_key INTEGER PRIMARY KEY,
+       trk_key INTEGER NOT NULL,
+       dset_key INTEGER NOT NULL,
+       search_range FLOAT NOT NULL,
+       steps INTEGER NOT NULL,
+       hist_binsINTEGER NOT NULL,
+       hist_range FLOAT NOT NULL,
+       FOREIGN KEY(dset_key) REFERENCES dsets(dset_key),
+       FOREIGN KEY(comp_key) REFERENCES comps(comp_key),
+       FOREIGN KEY(trk_key) REFERENCES tracking(comp_key)
+);
+
+CREATE TABLE vanHove (
+       comp_key INTEGER PRIMARY KEY,
+       dset_key INTEGER NOT NULL,
+       track_key INTEGER NOT NULL,
+       min_track_length INTEGER NOT NULL,
+       max_step INTEGER NOT NULL,
+       max_range FLOAT NOT NULL,
+       nbins INTEGER NOT NULL,
+       FOREIGN KEY(dset_key) REFERENCES dsets(dset_key),
+       FOREIGN KEY(comp_key) REFERENCES comps(comp_key),
+       FOREIGN KEY(track_key) REFERENCES tracking(comp_key)
+);
+
