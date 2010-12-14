ad1e0d7210201f21a3e0cef46382dea16bd1a7e3
Thomas A Caswell <tcaswell@uchicago.edu>
2010-12-14 15:56:12 -0600

    added script that makes a first pass at the r(T) plot for the paper

---
+++ scripts/paper_figs/make_part_size_plot.py
@@ -0,0 +1,25 @@
+import lib.general as gen
+import lib.plots as plts
+import lib.msd as lm
+
+conn = gen.open_conn("/home/tcaswell/colloids/proc_db.db")
+ce = conn.execute
+
+steps = [5,15,25]
+styles = ['x','^','o']
+
+msds = [ce("select comp_key from msd where date = '2010-12-08' and msd_steps = ?",(c,)).fetchall()
+        for c in steps]
+
+fits = [ [lm.fit_msd(c[0],conn) for c in m]
+         for m in msds]
+
+
+
+fig = plts.Figure('T [C]',r'r [$\mu$m]','NIPAM radius vs Temperature')
+
+for fts,sty,lab in zip(fits,styles,steps):
+    fig.plot([f[1] for f in fts],[f[2] for f in fts],sty,label=str(lab) + ' steps')
+
+
+fig.save('test')
