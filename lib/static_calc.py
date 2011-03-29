2ad9a16c4c1f45464c089274813a0145a772f28f
Thomas A Caswell <tcaswell@uchicago.edu>
2011-03-29 18:06:56 -0500

    Added function to do link_dumb on the same frame averaged differently.

--- lib/static_calc.py
+++ lib/static_calc.py
@@ -16,6 +16,10 @@
 #along with this program; if not, see <http://www.gnu.org/licenses>.
 from __future__ import division
 
+import h5py
+import lib.img as li
+import lib.static_calc as lsc
+import lib.vanHove as lvh
 import numpy as np
 
 
@@ -111,14 +115,62 @@ def link_dumb(h1,h2,m_range):
         cur_box_lst = h1.hash_table[cord_to_indx_2D(c,h1.hash_dims)]
         # get list of particles in current region in h1
         h2_region = h2.get_region(c,1)
-        for sp in cur_box_lst:
-            canidate_part = h2_region[np.argmin([dist(sp,p) for p in h2_region])]
-            
-            if dist(sp,canidate_part) < m_range:
-                pairs.append((sp,canidate_part))
+        if len(h2_region)>0:
+            for sp in cur_box_lst:
+                canidate_part = h2_region[np.argmin([dist(sp,p) for p in h2_region])]
+                
+                if dist(sp,canidate_part) < m_range:
+                    pairs.append((sp,canidate_part))
 
     return pairs
 
 def dist(a,b):
     return np.sum(np.sqrt((a-b)**2))
 
+
+
+
+
+def link_frame_avg(dset_key,conn,frame=25):
+    """ Takes in a dset_key a connection, and a frame in the single frame data set.
+
+    It then links all possible averaging combinations against each
+    other using link_dumb and returns arrays of the statistics on the
+    distribution.
+    """
+    ce = conn.execute
+    iden_lst = ce("select comp_key,frames_avged,fout from Iden where dset_key = ?",dset_key).fetchall()
+    F_lst = [h5py.File(i[2],'r') for i in iden_lst]
+    cut_pram = {'e_cut':.5,'rg_cut':15,'shift_cut':1.5}
+    xy_lst = [li.extract_centers(Fi,frame//c[1],c[0],cut_pram) for (Fi,c) in zip(F_lst,iden_lst)]
+
+    H_lst = []
+    # fill list of hash objects
+    for j in range(0,len(F_lst)):
+        H_lst.append(lsc.hash_case([1390,1040],10))
+
+    # fill hash object with particles
+    for h,xy in zip(H_lst,xy_lst):
+        for x,y in zip(*xy):
+           h.add_particle(np.array([x,y]))
+
+    res_std = np.zeros([len(H_lst),len(H_lst)])
+    res_mean = np.zeros([len(H_lst),len(H_lst)])
+    res_hwhm = np.zeros([len(H_lst),len(H_lst)])
+
+    # does the commutation
+    for j in range(0,len(H_lst)):
+        for k in range(j+1,len(H_lst)):
+            pairs = lsc.link_dumb(H_lst[j],H_lst[k],3)
+            res_mean[j][k] = np.mean([s[0]-e[0] for (s,e) in pairs] + [s[1]-e[1] for (s,e) in pairs])
+            res_std[j][k] = np.std([s[0]-e[0] for (s,e) in pairs] + [s[1]-e[1] for (s,e) in pairs])
+            res_hwhm[j][k] = lvh._vh_hwhm(*(np.histogram([s[0]-e[0] for (s,e) in pairs] + [s[1]-e[1] for (s,e) in pairs],1000,new=False)[::-1]))
+
+
+    for f in F_lst:
+        f.close()
+        del f
+
+    del F_lst
+
+    return res_std,res_mean,res_hwhm
